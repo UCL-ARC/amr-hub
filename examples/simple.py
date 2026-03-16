5b6d10dde14889e6f23635c3b18a333f4757c27f
Arindam Saha <arindamsaha1507@gmail.com>
2026-03-16 18:18:50 +0000

    Apply suggestions from code review
    Co-authored-by: Keiran Suchak <ksuchak1990@users.noreply.github.com>

--- examples/simple.py
+++ examples/simple.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 from amr_hub_abm.simulation_factory import create_simulation
 
@@ -7,8 +10,8 @@ from amr_hub_abm.simulation_factory import create_simulation
 def simulate(plot: bool = False) -> None:
     config_path = Path("tests/inputs/simulation_config.yml")
     simulation = create_simulation(config_path)
-    print([room.doors for room in simulation.space[0].floors[0].rooms])
-    print("Simulation created successfully...")
+    logger.info([room.doors for room in simulation.space[0].floors[0].rooms])
+    logger.info("Simulation created successfully...")
 
     while simulation.time < simulation.total_simulation_time:
         simulation.step(plot_path=Path("../simulation_outputs") if plot else None)
@@ -18,4 +21,4 @@ if __name__ == "__main__":
     time_start = time.perf_counter()
     simulate(plot=False)
     time_end = time.perf_counter()
-    print(f"Simulation run time: {time_end - time_start} seconds")
+    logger.info("Simulation run time: %s seconds", time_end - time_start)
