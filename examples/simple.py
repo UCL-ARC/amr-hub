5fb5295398ab01ca287d6f69b184df1a29e5c559
Arindam Saha <arindamsaha1507@gmail.com>
2026-03-19 13:01:38 +0000

    Preparing for profiling

--- examples/simple.py
+++ examples/simple.py
@@ -9,6 +9,6 @@ logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     time_start = time.perf_counter()
-    simulate(plot=False, record=True, plot_trajectory=True)
+    simulate(plot=False, record=False, plot_trajectory=False)
     time_end = time.perf_counter()
     logger.info("Simulation run time: %s seconds", time_end - time_start)
