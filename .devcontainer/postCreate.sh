ba4127f470e135a75d6726ead55f6597cd55c64b
Arindam Saha <arindamsaha1507@gmail.com>
2026-05-16 01:06:24 +0000

    Moved makefile to root

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,7 +4,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 export PATH="$HOME/.cargo/bin:$PATH"
 
-cd python-code/
+cd python-code/u
 uv sync --group dev
 uv run pre-commit install
 uv run pre-commit install --hook-type pre-push
