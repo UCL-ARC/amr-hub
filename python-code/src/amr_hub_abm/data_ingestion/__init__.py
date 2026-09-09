996c95a603de944b4eeeb928dfaa00518897d40d
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2026-09-09 14:58:09 +0100

    feat: resolve event location references
    Resolve patient and door location keys through supplied reference tables.
    
    Produce prepared model-location records and explicit reconciliation outputs
    without coupling ingestion to database access or TRE data sources.

--- python-code/src/amr_hub_abm/data_ingestion/__init__.py
+++ python-code/src/amr_hub_abm/data_ingestion/__init__.py
@@ -3,11 +3,21 @@
 from amr_hub_abm.data_ingestion.beds import BedLocation, extract_bed_location
 from amr_hub_abm.data_ingestion.doors import DoorLocation, extract_door_location
 from amr_hub_abm.data_ingestion.normalise import normalise_door_location_events
+from amr_hub_abm.data_ingestion.reference_locations import (
+    EventLocationResolutionStatus,
+    LocationResolutionReport,
+    resolve_door_location_events,
+    resolve_patient_location_events,
+)
 
 __all__ = [
     "BedLocation",
     "DoorLocation",
+    "EventLocationResolutionStatus",
+    "LocationResolutionReport",
     "extract_bed_location",
     "extract_door_location",
     "normalise_door_location_events",
+    "resolve_door_location_events",
+    "resolve_patient_location_events",
 ]
