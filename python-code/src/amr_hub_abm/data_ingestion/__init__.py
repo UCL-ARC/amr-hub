9202fe357811d259755e5c2e61ef6d17356cf850
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2026-09-16 13:59:00 +0100

    feat: prepare resolved location events

--- python-code/src/amr_hub_abm/data_ingestion/__init__.py
+++ python-code/src/amr_hub_abm/data_ingestion/__init__.py
@@ -3,6 +3,11 @@
 from amr_hub_abm.data_ingestion.beds import BedLocation, extract_bed_location
 from amr_hub_abm.data_ingestion.doors import DoorLocation, extract_door_location
 from amr_hub_abm.data_ingestion.normalise import normalise_door_location_events
+from amr_hub_abm.data_ingestion.prepared_events import (
+    EventPreparationStatus,
+    LocationEventPreparationReport,
+    prepare_location_events,
+)
 from amr_hub_abm.data_ingestion.reference_locations import (
     EventLocationResolutionStatus,
     LocationResolutionReport,
@@ -14,10 +19,13 @@ __all__ = [
     "BedLocation",
     "DoorLocation",
     "EventLocationResolutionStatus",
+    "EventPreparationStatus",
+    "LocationEventPreparationReport",
     "LocationResolutionReport",
     "extract_bed_location",
     "extract_door_location",
     "normalise_door_location_events",
+    "prepare_location_events",
     "resolve_door_location_events",
     "resolve_patient_location_events",
 ]
